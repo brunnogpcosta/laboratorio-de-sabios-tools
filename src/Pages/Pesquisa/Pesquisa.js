632104c8622216dbc0e99d53659fe913729026c7
Brunno Costa <brunno.costa@sotreq.com.br>
2021-02-09 19:23:58 -0300

    search filter done

--- src/Pages/Pesquisa/Pesquisa.js
+++ src/Pages/Pesquisa/Pesquisa.js
@@ -8,86 +8,81 @@ import imgCard from './img/graficos-tabelas-sao-objetos-basicos-para-estudo-esta
 import searchBt from './img/search.svg'
 
 import RightContent from '../../components/RightContent/RightContentComponent'
+
+let response;
+
 export default class Pesquisa extends Component {
-  state = {
-    cursos: [],
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      cursos: [],
+      cursosFiltrados: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
   }
 
   async componentDidMount() {
-    const response = await api.get('http://localhost:3333/allCourses')
+    response = await api.get('http://localhost:3333/allCourses')
     this.setState({ cursos: response.data.cursos })
+    this.setState({ cursosFiltrados: response.data.cursos })
+  }
+
+  handleSearch(termo) {
+    console.log("Termo: ", termo)
+    const arrayCursos = this.state.cursos;
+    this.setState({
+      cursosFiltrados: this.state.cursos.filter(curso => curso.nomeCurso.toLowerCase().indexOf(termo.toLowerCase()) > -1)
+
+    })
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+    this.handleSearch(event.target.value)
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    this.handleSearch(this.state.value)
+    event.preventDefault();
   }
 
 
-
-
-
   render() {
+
+    const { cursosFiltrados } = this.state
+
     return (
       <div id="pesquisaContent">
         <h2>Encontre seu Curso</h2>
 
         <label htmlFor="search">
           <img src={searchBt} />
-          <input type="search"></input>
+          <input type="search" value={this.state.value} onChange={this.handleChange}></input>
         </label>
-        <h3>28 Resultados Encontrados</h3>
+        <h3>{cursosFiltrados.length} Resultado(s) Encontrado(s)</h3>
 
         <div className="pesquisaContainer">
-          <a href="#">
-            <figure>
-              <img src={imgCard}></img>
-              <figcaption>
-                Estatística Fundamental
-            </figcaption>
-            </figure>
-          </a>
 
-          <a href="#">
-            <figure>
-              <img src={imgCard}></img>
-              <figcaption>
-                Estatística Fundamental
-            </figcaption>
-            </figure>
-          </a>
+          {cursosFiltrados.map(cursoFiltrado => (
+            <a href="#">
+              <figure>
+                <img src={imgCard}></img>
+                <figcaption>
+                  {cursoFiltrado.nomeCurso}
+                </figcaption>
+              </figure>
+            </a>
 
 
-          <a href="#">
-            <figure>
-              <img src={imgCard}></img>
-              <figcaption>
-                Estatística Fundamental
-            </figcaption>
-            </figure>
-          </a>
 
-          <a href="#">
-            <figure>
-              <img src={imgCard}></img>
-              <figcaption>
-                Estatística Fundamental
-            </figcaption>
-            </figure>
-          </a>
 
-          <a href="#">
-            <figure>
-              <img src={imgCard}></img>
-              <figcaption>
-                Estatística Fundamental
-            </figcaption>
-            </figure>
-          </a>
+          ))}
 
-          <a href="#">
-            <figure>
-              <img src={imgCard}></img>
-              <figcaption>
-                Estatística Fundamental
-            </figcaption>
-            </figure>
-          </a>
 
         </div>
 
