187d3ade20b4cd5b65adabdb271f05eef177afe1
Brunno Costa <brunnogpcosta@gmail.com>
2022-02-27 18:08:43 -0300

    finalização ferramenta cronometro

--- src/Pages/Ferramentas/cronometro/Cronometro.js
+++ src/Pages/Ferramentas/cronometro/Cronometro.js
@@ -1,96 +1,115 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './Cronometro.css'
 
 
-export default function Cronometro (){
+export default function Cronometro() {
 
     const [hora, setHora] = useState(0)
     const [minutos, setMinutos] = useState(0)
     const [segundos, setSegundos] = useState(0)
-    const [pausar, setPausar] = useState(false)
     const [iniciar, setIniciar] = useState(false)
     const [terminar, setTerminar] = useState(false)
+    const [corBotaoIniciar, setCorBotaoIniciar] = useState("#31a06c")
+    const [corFonte, setCorFonte] = useState("#fff")
+    const [ultimoTempo, setUltimoTempo] = useState("")
 
 
-        useEffect(() => {
-          handleCronometer()
-         
-            }, []);
-    
-   
-            function handleCronometer(){
-                console.log("entrou" + iniciar + pausar)
-                if(iniciar === true && pausar === false){
-                        const intervalId =  setInterval(() => {
-                            
-                            setSegundos(sec => sec + 1)
-                            trataCronometro()
-                        }, 1000);
-                        return () => clearInterval(intervalId);
-                 }
-                }
- 
 
-      function trataCronometro(){
-          
-            if(segundos === 60){
-                setMinutos(minutos + 1)
-                setSegundos(0)
-        
-            }
-
-            if(minutos === 60){
-                setHora(hora + 1)
-                setMinutos(0)
-        
-            }
-            if(hora === 24){
-                setHora(0)
-                setMinutos(0)
-                setSegundos(0)
-        
-            }
+    useEffect(() => {
+        //console.log("Iniciar: " + iniciar)
+        if (iniciar === true) {
+            const intervalId = setInterval(() => {
+                trataTempo()
+            }, 1000);
+            return () => clearInterval(intervalId);
         }
 
+    }, [iniciar]);
 
-function pausarCronometro(){
-    setPausar(true)
+
+
+
+    useEffect(() => {
+        trataCronometro()
+    }, [segundos]);
+
+    function trataTempo() {
+        setSegundos(sec => sec + 1)
+        trataCronometro()
+    }
+
+    function trataCronometro() {
+        //console.log("Segundos: " + segundos)
+
+        if (segundos === 60) {
+            setMinutos(minutos + 1)
+            setSegundos(0)
+
+        }
+
+        if (minutos === 60) {
+            setHora(hora + 1)
+            setMinutos(0)
+
+        }
+        if (hora === 24) {
+            setHora(0)
+            setMinutos(0)
+            setSegundos(0)
+
+        }
+    }
+
+
+
+    function iniciarCronometro() {
+        setTerminar(false)
+        setIniciar(iniciar === false ? true : false)
+        setCorFonte(iniciar === false ? "#000" : "#fff")
+        setCorBotaoIniciar(iniciar === false ? "#efefef" : "#31a06c")
+    }
+
+    function terminarCronometro() {
+        setUltimoTempo(hora +":"+minutos +":"+segundos)
+        setIniciar(false)
+        setTerminar(true)
+        setMinutos(0)
+        setHora(0)
+        setSegundos(0)
+
+    }
+
+
+
+    return (
+        <div id="cronometroContent">
+        <h3> Cronômetro </h3>
+
+{terminar&&
+            <div className='ultimoTempo' > Último Tempo: {ultimoTempo} </div> 
+} 
+{!terminar&&
+            <div className='ultimoTempo' >Tempo</div> 
 }
+           <div id="relogio">
 
-function iniciarCronometro(){
-    setIniciar(true)
-    handleCronometer()
-}
+                    <ul id="cronometro" >
+                        <li id="hora" > {hora < 10 ? "0" + hora : hora}:</li> 
+                        <li id="minutos" > {minutos < 10 ? "0" + minutos : minutos}:</li> 
+                        <li id="segundos" > {segundos < 10 ? "0" + segundos : segundos}</li> 
+                    </ul> 
+            </div>
 
-function terminarCronometro(){
-    setTerminar(true)
-}
+        <div id="botoes" >
+            <div className='meusBotoes'>
+                <button style={{ background: corBotaoIniciar, border: '1px solid #767676', color: corFonte }} onClick={iniciarCronometro} > {iniciar === false ? "Iniciar" : "Pausar"} </button> 
+                <button onClick={terminarCronometro} style={{ background: 'coral', border: '1px solid #767676', color: '#fff' }}> Terminar </button> 
+            </div> 
+        </div>
 
-
-
-return(
-    <div id="cronometroContent">
-    <h3>Cronômetro</h3>
-
-<div id="relogio">
- <ul id="cronometro">
-        <li id="hora">{hora}:</li>
-        <li id="minutos">{minutos}:</li>
-        <li id="segundos">{segundos}</li>
- </ul>
- </div>  
-
-<div id="botoes">
-    <div className='meusBotoes'>
-    <button onClick={iniciarCronometro}>Iniciar</button>
-    <button>Pausar</button>
-    <button>Terminar</button>
-    </div>
-</div>
-
-    </div>
-    )
+        </div>
+                                                    )
 
 
 }
